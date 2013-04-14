b9ba0f9c9b583e0deffe78a189eee3b1f74d9fd8
grosgg <marin.jeremy@gmail.com>
2013-04-14 22:39:57 +0200

    Test link with API

--- app/scripts/services/bookmark.js
+++ app/scripts/services/bookmark.js
@@ -2,7 +2,7 @@
 
 angular.module('bookmarkServices', ['ngResource'])
 .factory('Bookmark', function ($resource) {
-    return $resource('resources/bookmark.json', {}, {
+    return $resource('http://localhost:3000\:3000/bookmarks.json?authentication_token=bidon', {}, {
         query: {method:'GET', params:{}, isArray:true}
     });
 });
