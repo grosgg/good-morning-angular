c0badcdc40e34054839373b646c99719f779f73b
grosgg <marin.jeremy@gmail.com>
2013-05-15 00:47:34 +0200

    Add day and night modes

--- app/scripts/controllers/weather.js
+++ app/scripts/controllers/weather.js
@@ -5,18 +5,24 @@ goodMorningAngularApp.controller("WeatherCtrl", function($scope, $rootScope, $co
     var token = $cookieStore.get('authToken');
 
     // Get weather conditions and astronomy from backend api
-    $scope.weatherReport = WeatherReport.query({authToken:token});
+    WeatherReport.query({authToken:token}, function(data){
+        $scope.weatherReport = data;
 
-    currentTime = ($scope.weatherReport['astronomy']['current_time']['hour'] * 60) + $scope.weatherReport['astronomy']['current_time']['minute'];
-    sunriseTime = ($scope.weatherReport.astronomy.sunrise.hour * 60) + $scope.weatherReport.astronomy.sunrise.minute;
-    sunsetTime = ($scope.weatherReport.astronomy.sunset.hour * 60) + $scope.weatherReport.astronomy.sunset.minute;
-    console.log(current_time + ' ' + sunriseTime + ' ' + sunsetTime);
+        var currentTime = (parseInt(data.astronomy.current_time.hour) * 60) + parseInt(data.astronomy.current_time.minute);
+        var sunriseTime = (parseInt(data.astronomy.sunrise.hour) * 60) + parseInt(data.astronomy.sunrise.minute);
+        var sunsetTime = (parseInt(data.astronomy.sunset.hour) * 60) + parseInt(data.astronomy.sunset.minute);
+        console.log(currentTime + ' ' + sunriseTime + ' ' + sunsetTime);
 
-    if (currentTime >= sunriseTime && currentTime < sunsetTime) {
-        $rootScope.weatherBackground = 'day-background';
-    } else {
-        $rootScope.weatherBackground = 'night-background';
-    }
+        if (currentTime >= sunriseTime && currentTime < sunsetTime) {
+            $rootScope.backgroundMode = 'day-mode';
+            $rootScope.navbarMode = '';
+        } else {
+            $rootScope.backgroundMode = 'night-mode';
+            $rootScope.navbarMode = 'navbar-inverse';
+        }
+    });
+
+
 
 });
 
