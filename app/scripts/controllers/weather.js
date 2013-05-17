abcef9eb506116bf247beebf38b16064c287eae7
grosgg <marin.jeremy@gmail.com>
2013-05-17 15:31:05 +0200

    Disable weather astronomy call

--- app/scripts/controllers/weather.js
+++ app/scripts/controllers/weather.js
@@ -6,15 +6,17 @@ goodMorningAngularApp.controller("WeatherCtrl", function($scope, $rootScope, $co
 
     // Get weather conditions and astronomy from backend api
     WeatherReport.full({authToken:token}, function(data){
-        $scope.conditions = data.conditions;
-        $scope.astronomy = data.astronomy;
+        //$scope.conditions = data.conditions;
+        //$scope.astronomy = data.astronomy;
         $scope.hourly = data.hourly;
         $scope.forecast = data.forecast.simpleforecast.forecastday;
 
-        var currentTime = (parseInt(data.astronomy.current_time.hour) * 60) + parseInt(data.astronomy.current_time.minute);
-        var sunriseTime = (parseInt(data.astronomy.sunrise.hour) * 60) + parseInt(data.astronomy.sunrise.minute);
-        var sunsetTime = (parseInt(data.astronomy.sunset.hour) * 60) + parseInt(data.astronomy.sunset.minute);
-        //console.log(currentTime + ' ' + sunriseTime + ' ' + sunsetTime);
+        /*
+         *var currentTime = (parseInt(data.astronomy.current_time.hour) * 60) + parseInt(data.astronomy.current_time.minute);
+         *var sunriseTime = (parseInt(data.astronomy.sunrise.hour) * 60) + parseInt(data.astronomy.sunrise.minute);
+         *var sunsetTime = (parseInt(data.astronomy.sunset.hour) * 60) + parseInt(data.astronomy.sunset.minute);
+         *console.log(currentTime + ' ' + sunriseTime + ' ' + sunsetTime);
+         */
 
         /*
          *if (currentTime >= sunriseTime && currentTime < sunsetTime) {
