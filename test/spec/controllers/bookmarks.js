d0949fa608f57da0f783bf0cfe2fa4f4027669ea
grosgg <marin.jeremy@gmail.com>
2013-05-18 20:37:38 +0200

    Bookmarks controller unit tests

--- test/spec/controllers/bookmarks.js
+++ test/spec/controllers/bookmarks.js
@@ -2,6 +2,37 @@
 
 describe('Controller: BookmarksCtrl', function() {
 
-    beforeEach(module('bookmarkServices'));
+    var scope, ctrl, $httpBackend;
+
+    beforeEach(module('goodMorningAngularApp'));
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+        $httpBackend = _$httpBackend_;
+        $httpBackend.expectGET('http://localhost:3000/bookmarks.json?authentication_token=').
+            respond([
+                {"id":1,"title":"Sabu Web","url":"http://sabu.fr","description":"Our wedding website"},
+                {"id":2,"title":"Google","url":"http://www.google.fr","description":"The Search Engine"},
+                {"id":3,"title":"Japon 365","url":"http://www.japon365.com","description":"Photo Blog about Japan"}
+            ]);
+
+        scope = $rootScope.$new();
+        ctrl = $controller('BookmarksCtrl', {$scope: scope});
+    }));
+
+    it('should create "bookmarks" model with 3 bookmarks fetched from xhr', function() {
+        expect(scope.bookmarks).toEqual([]);
+        $httpBackend.flush();
+
+        expect(scope.bookmarks.length).toBe(3);
+    });
+
+
+    it('should set the default value of orderBookmarks model', function() {
+        expect(scope.orderBookmarks).toBe('id');
+    });
+
+    it('should set the default value of reverseBookmarks model', function() {
+        expect(scope.reverseBookmarks).toBe(true);
+    });
 
 });
