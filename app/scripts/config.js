559930efcecc20269778522ba4cdc6c96d4dbe72
Jeremy Marin <marin.jeremy@gmail.com>
2013-07-11 23:38:47 +0200

    Fix config.js

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,19 +1,19 @@
 var url = window.document.URL;
 if (url.indexOf('localhost:9000') != -1 || url.indexOf('localhost:8080') != -1) {
-    backendUrl = 'http://localhost\\:3000/';
-    usernamePrefill = 'demo@sabu.fr';
-	passwordPrefill = 'password';
+    var backendUrl = 'http://localhost\\:3000/';
+    var usernamePrefill = 'demo@sabu.fr';
+	var passwordPrefill = 'password';
 } else if (url.indexOf('192.168.1.91') != -1) {
-    backendUrl = 'http://192.168.1.91\\:8016/';
-    usernamePrefill = 'marin.jeremy@gmail.com';
-	passwordPrefill = '';
+    var backendUrl = 'http://192.168.1.91\\:8016/';
+    var usernamePrefill = 'marin.jeremy@gmail.com';
+	var passwordPrefill = '';
 } else if (url.indexOf('grosgg.no-ip.org:8015') != -1) {
-    backendUrl = 'http://grosgg.no-ip.org\\:8016/';
-    usernamePrefill = 'marin.jeremy@gmail.com';
-	passwordPrefill = '';
+    var backendUrl = 'http://grosgg.no-ip.org\\:8016/';
+    var usernamePrefill = 'marin.jeremy@gmail.com';
+	var passwordPrefill = '';
 } else {
-	backendUrl = 'http://grosgg.no-ip.org\\:8026/';
-    usernamePrefill = 'demo@sabu.fr';
-	passwordPrefill = 'password';
+	var backendUrl = 'http://grosgg.no-ip.org\\:8026/';
+    var usernamePrefill = 'demo@sabu.fr';
+	var passwordPrefill = 'password';
 }
 
