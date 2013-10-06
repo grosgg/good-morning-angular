747ce390d04742a7529e1a47af01e073f742ffad
Jeremy Marin <marin.jeremy@gmail.com>
2013-10-06 21:00:15 +0200

    Move hosting to Heroku

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -7,13 +7,8 @@ if (url.indexOf('localhost:9000') != -1 || url.indexOf('localhost:8080') != -1) 
     var backendUrl = 'http://192.168.1.91\\:8016/';
     var usernamePrefill = 'marin.jeremy@gmail.com';
 	var passwordPrefill = '';
-} else if (url.indexOf('grosgg.no-ip.org:8015') != -1) {
-    var backendUrl = 'http://grosgg.no-ip.org\\:8016/';
+} else {
+    var backendUrl = 'http://backend.good-morning.sabu.fr/';
     var usernamePrefill = 'marin.jeremy@gmail.com';
 	var passwordPrefill = '';
-} else {
-	var backendUrl = 'http://grosgg.no-ip.org\\:8026/';
-    var usernamePrefill = 'demo@sabu.fr';
-	var passwordPrefill = 'password';
 }
-
